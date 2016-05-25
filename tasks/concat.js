9524d2f79736e88637f40301b73590e5a664d5c5
hsmsharique <shssuetce@gmail.com>
2016-05-25 10:45:17 +0500

    Release v0.0.10

--- tasks/concat.js
+++ tasks/concat.js
@@ -3,7 +3,7 @@ module.exports = {
     separator: ';'
   },
   dist: {
-    src: ['src/lodash.custom.min.js', 'src/ng-access-control.js', 'src/directive/control.directive.js'],
+    src: ['src/ng-access-control.js', 'src/directive/control.directive.js'],
     dest: '.tmp/ng-access-control.js'
   }
 };
